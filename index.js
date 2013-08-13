9b57d479f1e4c86c7f04dcd4b0f4cd943ca2e4f1
Raynos <raynos2@gmail.com>
2013-08-13 16:56:54 -0700

    added re-export to index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var maybeCallback = require("./maybe-callback")
-maybeCallback.chain = require("./chain")
-maybeCallback.error = require("./error")
-maybeCallback.join = require("./join")
-maybeCallback.map = require("./map")
-maybeCallback.of = require("./of")
-maybeCallback.either = require("./either")
-maybeCallback.to = require('./to')
+var maybeCallback = require("./maybe-callback.js")
+maybeCallback.both = require("./both.js")
+maybeCallback.chain = require("./chain.js")
+maybeCallback.either = require("./either.js")
+maybeCallback.error = require("./error.js")
+maybeCallback.join = require("./join.js")
+maybeCallback.mapAsync = require("./map-async.js")
+maybeCallback.map = require("./map.js")
+maybeCallback.of = require("./of.js")
+maybeCallback.to = require("./to.js")
 
 module.exports = maybeCallback
 
