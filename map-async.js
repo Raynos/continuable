81472726e69c88b53f4908e989e7d45a2d198c71
Joshua T Corbin <joshua@uber.com>
2014-04-27 18:34:28 -0700

    mapAsync dreamt of being a bind application

--- map-async.js
+++ map-async.js
@@ -2,13 +2,9 @@ module.exports = mapAsync
 
 // mapAsync := (Continuable<A>, lambda: (A, Callback<B>)) => Continuable<B>
 function mapAsync(source, lambda) {
-    return function continuable(callback) {
-        source(function continuation(err, value) {
-            if (err) {
-                return callback(err)
-            }
-
+    bind(source, function(value) {
+        return function continuable(callback) {
             lambda(value, callback)
-        })
-    }
+        }
+    })
 }
